b662c73e19917bd42475e9063f2d23b96cb8c0f1
MikeC711 <mikec@casilenc.com>
2014-09-05 13:23:39 -0400

    More stripped down version

--- com/ibm/mike/samples/SimpleHttpClientDriver.java
+++ com/ibm/mike/samples/SimpleHttpClientDriver.java
@@ -2,10 +2,6 @@ package com.ibm.mike.samples;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -28,8 +24,6 @@ import org.apache.http.util.EntityUtils;
  *  Simplified version of threaded to handle Basic Authentication
  */
 public class SimpleHttpClientDriver {		// Logger, so logging.properties to control tracing
-	private static final String thisClass = SimpleHttpClientDriver.class.getName() ;
-	private static Logger hcdLogger = Logger.getLogger(thisClass) ;
 	
 	private static PoolingHttpClientConnectionManager cm = null ;
 	private static CloseableHttpClient httpClient = null ;
@@ -41,18 +35,15 @@ public class SimpleHttpClientDriver {		// Logger, so logging.properties to contr
 	private static int authPort ;
 				// List of URIs that I will hit
 	private final static String [] URIs = { "/CTLBankWeb/welcome.jsp", 
-		"/CTLBankWeb/App?action=login&uid={uid}&passwd=password", "/CTLBankWeb/App?action=acctSummary",
-	    "/CTLBankWeb/App?action=Deposit&amount={amt}&accID={uid}01", "/CTLBankWeb/App?action=Withdraw&amount={amt}&accID={uid}01",
-	    "/CTLBankWeb/App?action=custProfile", "/CTLBankWeb/App?action=logout" } ;
+		"/CTLBankWeb/App?action=login&uid=3&passwd=password", "/CTLBankWeb/App?action=acctSummary" } ;
 			// Userids and passwords to create credentials
-	private static final String [] uidList = {"mobusr1", "mobusr2", "webusr1", "webusr2", "webusr3", "webusr4", "webusr5" } ;
-	private static final String [] pwList = {"mobusr1", "mobusr2", "webusr1", "webusr2", "webusr3", "webusr4", "webusr5" } ;
+	private static final String [] uidList = {"mobusr1", "webusr1" } ;
+	private static final String [] pwList = {"mobusr1", "webusr1" } ;
 
 	/**
 	 * This is the thread started for each virtual user
 	 */
 	private class OneUser extends Thread {
-		String dbUid, dbAmt ;
 		HttpClientContext httpClientContext = null ;
 
 		/**
@@ -63,9 +54,7 @@ public class SimpleHttpClientDriver {		// Logger, so logging.properties to contr
 		 * @param urlInfo Double String Array w/URI info (not used for requests which have no var subst
 		 * @param baseURIs For requests w/no variable substitution, URI is built once only
 		 */
-		OneUser(int thdIdx, int iUid, int iAmt, String sUid, String sPw, AuthScope authScope) {
-			dbUid = String.valueOf(iUid) ;
-			dbAmt = String.valueOf(iAmt) ;
+		OneUser(int thdIdx, String sUid, String sPw, AuthScope authScope) {
 			httpClientContext = HttpClientContext.create() ;		// Unique clientContext per thread
 
 			CredentialsProvider  credentialsProvider = new BasicCredentialsProvider() ;	// Credentials per thread
@@ -77,51 +66,22 @@ public class SimpleHttpClientDriver {		// Logger, so logging.properties to contr
 			authCache.put(new HttpHost(authScope.getHost(), authScope.getPort()), basicScheme) ;
 				
 			httpClientContext.setAuthCache(authCache);
-			if (hcdLogger.isLoggable(Level.FINER))
-				hcdLogger.logp(Level.FINER, thisClass+"OneUser", "constructor",	"Provider: {0}  httpCliCtx {1}",
-					new Object [] { credentialsProvider.getCredentials(authScope).getUserPrincipal(),
-					httpClientContext.getCredentialsProvider().getCredentials(AuthScope.ANY).getUserPrincipal()});
 		}
 		
 		public void run() {
-			final String METHOD = this.getName()+"run" ;
 			try {
 				String hostSpec = "http://"+authHost+":"+authPort ;
 				for (String curURIStr : URIs) {
-					while ((curURIStr.indexOf("{uid}")) > -1)
-						curURIStr = curURIStr.replace("{uid}", dbUid) ;
-					while ((curURIStr.indexOf("{amt}")) > -1)
-						curURIStr = curURIStr.replace("{amt}", dbAmt) ;
 					URI curURI = new URIBuilder(hostSpec+curURIStr).build() ;
 	       			CloseableHttpResponse httpResponse = handleRetryableRequest(curURI, 0) ;
 	       			if (httpResponse == null)  continue ;		// Exception/error logged in called method
 	       			HttpEntity htEntity = null ;
-	       			try {
-						htEntity = httpResponse.getEntity() ;
-						if (hcdLogger.isLoggable(Level.FINE)) {
-							hcdLogger.logp(Level.FINE, thisClass, METHOD, "URI: {0}  StatusCd: {1}", 
-								new Object [] { curURI, httpResponse.getStatusLine().getStatusCode()}) ;
-							if (hcdLogger.isLoggable(Level.FINER)) {
-								for (Header curHdr : httpResponse.getAllHeaders()) {
-									hcdLogger.logp(Level.FINER, thisClass, METHOD, "httpResponse header: ", curHdr.getElements()) ; 
-								}
-								hcdLogger.logp(Level.FINER, thisClass, METHOD, "Results: {0}", 
-									(htEntity != null) ? EntityUtils.toString(htEntity) : "NoEntity") ;
-							}
-						}
-						if (htEntity != null)  EntityUtils.consume(htEntity);
-	       			} catch (Exception e) {
-	    				hcdLogger.logp(Level.WARNING, thisClass, METHOD, "Exception wrappering get request. URI: {0}  Exception: {1}",
-	    						new Object [] { curURI, e }) ;
-	    				e.printStackTrace();
-	       			} finally {
-	    				try { 
-	    					if (httpResponse != null)  {
-	    						if (htEntity != null)  EntityUtils.consume(htEntity);
-	    						httpResponse.close();	
-	    					} 
-	    				}catch (IOException e) { }
-	       			}
+					htEntity = httpResponse.getEntity() ;
+					if (htEntity != null)  EntityUtils.consume(htEntity);
+   					if (httpResponse != null)  {
+   						if (htEntity != null)  EntityUtils.consume(htEntity);
+   						httpResponse.close();	
+   					} 
 				}
 			} catch (Exception e) {
 				System.out.println("Caught exception : "+e);
@@ -130,30 +90,17 @@ public class SimpleHttpClientDriver {		// Logger, so logging.properties to contr
 		}
 
 		private CloseableHttpResponse handleRetryableRequest(URI curURI, int retryCnt) {
-			final String METHOD = this.getName()+"handleRetryableRequest" ;
 			try {
 				HttpGet httpGet = new HttpGet(curURI) ;
 				httpGet.setHeader("Connection", "keep-alive");					
-				if (hcdLogger.isLoggable(Level.FINER)) {
-					for (Header curHdr : httpGet.getAllHeaders()) {
-						hcdLogger.logp(Level.FINER, thisClass, METHOD, "httpGet header: ", curHdr.getValue()) ; 
-					}
-				}			// execute w/context local to the thread
 				CloseableHttpResponse htResp = httpClient.execute(httpGet, httpClientContext) ;
-				if (hcdLogger.isLoggable(Level.FINER))
-					hcdLogger.logp(Level.FINER, thisClass+"OneUser", METHOD, "statusCd: {0} httpCliCtx {1}",
-						new Object [] { htResp.getStatusLine().getStatusCode(), 
-						httpClientContext.getCredentialsProvider().getCredentials(authScope).getUserPrincipal()});
 
 				return htResp ;
 			} catch (Exception e) {
 				if (retryCnt < 2) {	// Route exceptions, retry twice only
-					hcdLogger.logp(Level.INFO, thisClass, METHOD, "RetryCnt: {0} curURI: {1}",
-						new Object [] { retryCnt, curURI}) ;
 					return handleRetryableRequest(curURI, ++retryCnt) ;
 				} else
-					hcdLogger.logp(Level.WARNING, thisClass, METHOD, "Exception executing get request. URI: {0}  Exception: {1}",
-						new Object [] { curURI, e }) ;
+					e.printStackTrace();
 			}
 			return null ;
 		}
@@ -168,47 +115,36 @@ public class SimpleHttpClientDriver {		// Logger, so logging.properties to contr
 	 * and interval between can be tweaked w/the parms. Using minutes so monitoring thread can do it when awake
 	 */
 	public static void main(String [] args) {
-		final String METHOD = "main" ;
 		SimpleHttpClientDriver me = new SimpleHttpClientDriver() ;
-		if (hcdLogger.isLoggable(Level.FINE))
-			hcdLogger.logp(Level.FINE, thisClass, METHOD, "parms optional: <host> <port>") ;
 		authHost = (args.length > 0) ? args[0] : authHostDflt ;
 		authPort = (args.length > 1) ? Integer.parseInt(args[1]) : authPortDflt ;
-		if (hcdLogger.isLoggable(Level.FINE))
-			hcdLogger.logp(Level.FINE, thisClass, METHOD, "Starting w/host: {0} Port: {1}",
-				new Object [] { authHost, authPort}) ;
 		OneUser [] wrkThreads = new OneUser[uidList.length] ;
 		try {
 			authScope = new AuthScope(authHost, authPort) ;
 			me.setupHttpClient() ;		// Set up client, then construct/init/start each thread
 			for (int i = 0; i < uidList.length; i++) {
-				wrkThreads[i] = me.new OneUser(i, i, i+10, uidList[i], pwList[i], authScope) ;
+				wrkThreads[i] = me.new OneUser(i, uidList[i], pwList[i], authScope) ;
 				wrkThreads[i].setName("WrkThread:"+i);
 				wrkThreads[i].start();
 			}
 			wrkThreads[uidList.length-1].join();					// Sleep until last thd completes
 		} catch (Exception e) {
-			hcdLogger.logp(Level.SEVERE, thisClass, METHOD, "Exception in main processing: {0}", e) ;
 			e.printStackTrace();
 		}
 	}
 
 	
 	public void setupHttpClient() {
-		final String METHOD = "setupHttpClient" ;
 		cm = new PoolingHttpClientConnectionManager();
 		cm.setMaxTotal(256);
 		cm.setDefaultMaxPerRoute(128) ;	
 		httpClient = HttpClients.createMinimal(cm) ;
-		if (hcdLogger.isLoggable(Level.FINER))
-			hcdLogger.logp(Level.FINER, thisClass, METHOD, "Constructing cm/httpClient for cbc obj: {0}", this) ;
 	}
 
 	protected void finalize() {
 		try {
 			super.finalize() ;
 		} catch (Throwable e) {
-			hcdLogger.logp(Level.WARNING, thisClass, "finalize", "Exception in super.finalize(): {0}", e) ;
 			e.printStackTrace();
 		}
 		if (httpClient != null)		try {	httpClient.close() ;  } catch (IOException e) { }
